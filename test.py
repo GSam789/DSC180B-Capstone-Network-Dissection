1e9aefb8eff84eb3d0a7513cd39266f5607ff3f1
kent831 <kutama@ucsd.edu>
2023-02-12 18:22:20 -0800

    Added test data

--- test.py
+++ test.py
@@ -1,11 +1,10 @@
 from getdata import get_data
 from train import train, validate
 import resnet18 
-import dropout
 import pickle
 import torch
 import torch.nn as nn
-import torch.optim as optim
+from torch.utils.data import DataLoader
 
 # Load Model & Data
 
@@ -14,12 +13,18 @@ import torch.optim as optim
 filename = "models/resnet18_cifar10_dropout0.2"
 # filename = "models/resnet18_cifar10_dropout0.5"
 
+# Load Test Data (First 100 CIFAR10 validation data)
 model = pickle.load(open(filename, "rb"))
-train_loader, valid_loader = get_data()
-
-# Test Data
-optimizer = optim.Adam(model.parameters(), lr=0.0001)
+test_data = torch.load("test_data.pt")
 loss = nn.CrossEntropyLoss()
 
+valid_loader = DataLoader(
+        test_data, 
+        batch_size=16,
+        shuffle=False)
+
+# Test model accuracy
+model_name = filename.split("/")[1]
+print(f"Model: {model_name}")
 val_loss, val_acc = validate(model, valid_loader, loss, 'cpu')
 print(f"Validation loss: {val_loss}, Validation accuracy: {val_acc}")
