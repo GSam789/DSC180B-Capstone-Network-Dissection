7e28c3792f9825b932474b4c4ab8ad14792ba04c
kent831 <kutama@ucsd.edu>
2023-02-13 16:36:01 -0800

    FocusedDropout

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 from getdata import get_data
 from train import train, validate
-import resnet18 
-import dropout
+import resnet18 as rn
+import dropout_model as dm
+import focuseddropout_model as fm
 import pickle
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import matplotlib.pyplot as plt
+from plotting import plot
 
 # Load Model & Data
 
+# model_name = "models/resnet18_cifar10"
+# model_name = "models/resnet18_cifar10_dropout0.2"
+model_name = "resnet18_cifar10_dropout0.5"
+filename = "models/" + model_name
+
+device = 'cpu'
+if torch.cuda.is_available():
+    device = 'cuda'
+
 # ResNet
-# model = resnet18.ResNet(img_channels = 3, num_layers = 18, block = resnet18.BasicBlock, num_classes = 10)
+# model = rn.ResNet(img_channels = 3, num_layers = 18, block = resnet18.BasicBlock, num_classes = 10)
 
 # ResNet with dropout
-model = dropout.ResNet(img_channels = 3, num_layers = 18, block = dropout.BasicBlock, num_classes = 10, dropout_rate = 0.2)
-train_loader, valid_loader = get_data()
+# model = dm.ResNet(img_channels = 3, num_layers = 18, block = dropout.BasicBlock, num_classes = 10, dropout_rate = 0.2)
+
+# ResNet with FocusedDropout
+model = fm.ResNet(img_channels = 3, num_layers = 18, block = dropout.BasicBlock, num_classes = 10, par_rate = 0.1)
 
 # Train Data
+train_loader, valid_loader = get_data()
 optimizer = optim.Adam(model.parameters(), lr=0.0001)
 loss = nn.CrossEntropyLoss()
+train_losses, train_accs, val_losses, val_accs = [], [], [], []
+
 for _ in range(10):
     train_loss, train_acc = train(model, train_loader, optimizer, loss, 'cpu')
+    train_losses.append(train_loss)
+    train_accs.append(train_acc)
+    
+    val_loss, val_acc = validate(model, valid_loader, loss, device)
+    val_losses.append(val_loss)
+    val_accs.append(val_acc)
     print(f"Training loss: {train_loss}, Training accuracy: {train_acc}")
+    print(f"Validation loss: {val_loss}, Validation accuracy: {val_acc}")
 print("Training Done!")
-
-val_loss, val_acc = validate(model, valid_loader, loss, 'cpu')
-print(f"Validation loss: {val_loss}, Validation accuracy: {val_acc}")
+plot(train_losses, train_accs, val_losses, val_accs, model_name)
 
 # Save Model
-# filename = "models/resnet18_cifar10"
-# filename = "models/resnet18_cifar10_dropout0.2"
-filename = "models/resnet18_cifar10_dropout0.5"
 pickle.dump(model, open(filename, "wb"))
 print(f"Model saved on {filename}")
---
+++ plotting.py
@@ -0,0 +1,23 @@
+def plot(train_loss, train_acc, val_loss, val_acc):
+    x = range(1, 1+len(train_loss))
+    fig, (ax1,ax2) = plt.subplots(1,2)
+    fig.suptitle(f'Model: {model}')
+    ax1.plot(x, train_loss, label = "Training loss")
+    ax1.plot(x, val_loss, label = "Test loss")
+
+    ax1.set_title("Training & Validation loss")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+
+    ax2.plot(x, train_acc, label = "Training acc")
+    ax2.plot(x, val_acc, label = "Test acc")
+    ax2.set_title("Training & Validation Accuracy")
+    ax2.set_xlabel("Epochs")
+    ax2.set_ylabel("Accuracy")
+    
+    ax1.legend()
+    ax2.legend()
+
+    fig.tight_layout(pad = 0.5)
+    plt.savefig(f"plots/{model}.png")
+    plt.show()
