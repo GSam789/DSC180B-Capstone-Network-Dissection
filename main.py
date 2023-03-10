e73491af8716d66ab043a9883c14f036d033db11
kent831 <kutama@ucsd.edu>
2023-03-10 00:51:56 +0000

    .pth files

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from getdata import get_data
 from train import train, validate
-from utils import resnet18 as rn
-from utils import dropout_model as dm
-from utils import focuseddropout_model as fm
-from utils import antifdo_model as afm
+# from utils import resnet18 as rn
+# from utils import dropout_model as dm
+# from utils import focuseddropout_model as fm
+# from utils import antifdo_model as afm
 from utils.vgg16 import VGG
 from utils import vgg16_dropout as vd
 from utils import vgg16_focuseddropout as vfd
@@ -35,21 +35,21 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 # model_name = "resnet18_cifar100_focuseddropout0.05sgd"
 
 ############# VGG16
-# model = VGG('VGG16', num_classes = 100)
-# model_name = "cifar100/vgg/vgg16_cifar100"
+model = VGG('VGG16', num_classes = 100)
+model_name = "vgg16_cifar100"
 
 # ############# VGG16 with Dropout
-model = vd.VGG('VGG16', num_classes = 100)
-model_name = "cifar100/vgg/vgg16_cifar100_dropout0.2"
+# model = vd.VGG('VGG16', num_classes = 100)
+# model_name = "cifar100/vgg/vgg16_cifar100_dropout0.2"
 
 # ############# VGG16 with FocusedDropout
 # model = vfd.VGG('VGG16', num_classes = 100)
 # model_name = "cifar100/vgg/vgg16_cifar100_focuseddropout0.1"
 
-filename = "models/" + model_name
+filename = "models/" + model_name + '.pth'
 # model = pickle.load(open(filename, "rb"))
 ########################################Train & Validate#####################################################
-epochs = 300
+epochs = 150
 train_loader, valid_loader = get_data(batch_size=128)
 
 model.to(device)
@@ -75,11 +75,14 @@ for _ in range(epochs):
     scheduler.step()
     print(f"Training loss: {train_loss}, Training accuracy: {train_acc}")
     print(f"Validation loss: {val_loss}, Validation accuracy: {val_acc}")
+    
 print("Training Done!")
 print("Best training accuracy: ", max(train_accs))
 print("Best validation accuracy: ", max(val_accs))
 plot(train_losses, train_accs, val_losses, val_accs, model_name)
 
 # Save Model
-pickle.dump(model, open(filename, "wb"))
+torch.save(model.state_dict(), filename)
+# torch.save(model, filename)
+# pickle.dump(model, open(filename, "wb"))
 print(f"Model saved on {filename}")
